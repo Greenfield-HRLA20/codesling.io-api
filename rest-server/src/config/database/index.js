e959780e8544d4090ce9023ae5c41f11c54c176e
Justin Dai <justingdai@gmail.com>
2018-03-07 09:47:22 -0800

    [Setup] BuildEnv and database setup

--- rest-server/src/config/database/index.js
+++ rest-server/src/config/database/index.js
@@ -3,23 +3,35 @@ require('dotenv').config();
 import { Pool } from 'pg';
 import Promise from 'bluebird';
 
-import { 
-  success,
-  error,
-} from '../../lib/log';
+import { success, error } from '../../lib/log';
 
 /**
- * config for pg pool 
+ * config for pg pool
  * limiting number of connections to 20
  * must end connection after each query => database.end()
  */
 
 const config = {
-  user: process.env.NODE_ENV === 'production' ? process.env.AWS_USER : process.env.LOCAL_USER,
-  host: process.env.NODE_ENV === 'production' ? process.env.AWS_HOST : process.env.LOCAL_HOST,
-  database: process.env.NODE_ENV === 'production' ? process.env.AWS_DATABASE : process.env.LOCAL_DATABASE,
-  password: process.env.NODE_ENV === 'production' ? process.env.AWS_PASSWORD : process.env.LOCAL_PASSWORD,
-  port: process.env.NODE_ENV === 'production' ? process.env.AWS_PORT : process.env.LOCAL_PORT,
+  user:
+    process.env.NODE_ENV === 'production'
+      ? process.env.AWS_USER
+      : process.env.LOCAL_USER,
+  host:
+    process.env.NODE_ENV === 'production'
+      ? process.env.AWS_HOST
+      : process.env.LOCAL_HOST,
+  database:
+    process.env.NODE_ENV === 'production'
+      ? process.env.AWS_DATABASE
+      : process.env.LOCAL_DATABASE,
+  password:
+    process.env.NODE_ENV === 'production'
+      ? process.env.AWS_PASSWORD
+      : process.env.LOCAL_PASSWORD,
+  port:
+    process.env.NODE_ENV === 'production'
+      ? process.env.AWS_PORT
+      : process.env.LOCAL_PORT,
   max: 20
 };
 
@@ -31,13 +43,13 @@ db.on('connect', () => {
 
 // db.on('acquire', client => {
 //   success('successfully acquired ', Object.keys(client));
-// })
+// });
 
 db.on('remove', client => {
-  success('successfully removed client= ', client)
+  success('successfully removed client= ', client);
 });
 
-db.on('error', (err) => {
+db.on('error', err => {
   error('error in pg ', err);
 });
 
