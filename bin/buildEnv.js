0fa866e25b365ba49add8a63051801371f1b1156
Jason <jsato03@gmail.com>
2018-03-07 09:55:02 -0800

    Master rebase

--- bin/buildEnv.js
+++ bin/buildEnv.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const envVariables = require('../config/.env.sample');
 
 const createENVFile = (directory, variables) => {
+<<<<<<< HEAD
   _.each(variables, variable => {
     fs.appendFileSync(`./${directory}/.env`, variable + '\n');
   });
@@ -15,5 +16,18 @@ const buildEnv = () => {
     createENVFile(key, value);
   });
 };
+=======
+  _.each(variables, (variable) => {
+    fs.appendFileSync(`./${directory}/.env`, variable + '\n');
+  })
+}
+
+const buildEnv = () => {
+  _.each(envVariables, (value, key) => {
+    fs.writeFileSync(`./${key}/.env`, '')
+    createENVFile(key, value);
+  });
+}
+>>>>>>> Master rebase
 
 buildEnv();
